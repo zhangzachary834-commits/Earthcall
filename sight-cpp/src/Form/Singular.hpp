f4c8cb505a5cc6e0a56bed556e97ddd40f227c0d
zhangzachary834-commits <zhangzachary834@gmail.com>
2025-08-11 19:02:48 -0700

    new update

--- sight-cpp/src/Form/Singular.hpp
+++ sight-cpp/src/Form/Singular.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <string>
 
+// ------------------------------------------------------------------
+// A Singular represents any entity or concept with meaning greater than the sum of its parts.
 // Marker interface representing a "singular being" in the system.
 // Any class that wishes to participate in Relation should inherit
 // from this interface and provide a stable textual identifier
 // via getIdentifier().
+// ------------------------------------------------------------------
 class Singular {
 public:
     virtual ~Singular() = default;
