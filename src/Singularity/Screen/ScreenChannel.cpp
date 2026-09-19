fcf88a329afa624563addabfa960986498275fd8
zhangzachary834-commits <zhangzachary834@gmail.com>
2026-09-19 11:49:12 -0700

    screen: register SDF cache telemetry properties

--- src/Singularity/Screen/ScreenChannel.cpp
+++ src/Singularity/Screen/ScreenChannel.cpp
@@ -56,7 +56,9 @@ ScreenChannel* ScreenChannel::find(LawManager& laws) {
 
 void ScreenChannel::updateMetrics(int dCalls, int tris, double vramBytes,
                                  double uBytes, int suballocs, int pipeSwitches,
-                                 int cachedMeshes) {
+                                 int cachedMeshes, int sdfCompiles,
+                                 int sdfCacheHits, int sdfCacheMisses,
+                                 double sdfWgslBytes, double sdfParamBytes) {
     drawCalls = dCalls;
     trianglesDrawn = tris;
     vramAllocatedBytes = vramBytes;
@@ -64,6 +66,11 @@ void ScreenChannel::updateMetrics(int dCalls, int tris, double vramBytes,
     bufferSuballocations = suballocs;
     pipelineSwitches = pipeSwitches;
     cachedMeshesCount = cachedMeshes;
+    sdfProgramCompiles = sdfCompiles;
+    sdfProgramCacheHits = sdfCacheHits;
+    sdfProgramCacheMisses = sdfCacheMisses;
+    sdfWgslBytesGenerated = sdfWgslBytes;
+    sdfParameterBytesUploaded = sdfParamBytes;
 }
 
 void ScreenChannel::buildProperties() {
@@ -97,6 +104,11 @@ void ScreenChannel::buildProperties() {
     readOnlyInt("bufferSuballocations", &ScreenChannel::getBufferSuballocations);
     readOnlyInt("pipelineSwitches", &ScreenChannel::getPipelineSwitches);
     readOnlyInt("cachedMeshesCount", &ScreenChannel::getCachedMeshesCount);
+    readOnlyInt("sdfProgramCompiles", &ScreenChannel::getSdfProgramCompiles);
+    readOnlyInt("sdfProgramCacheHits", &ScreenChannel::getSdfProgramCacheHits);
+    readOnlyInt("sdfProgramCacheMisses", &ScreenChannel::getSdfProgramCacheMisses);
+    readOnlyDouble("sdfWgslBytesGenerated", &ScreenChannel::getSdfWgslBytesGenerated);
+    readOnlyDouble("sdfParameterBytesUploaded", &ScreenChannel::getSdfParameterBytesUploaded);
     boolean("wireframe", &ScreenChannel::wireframe);
     boolean("heightGridDdaEnabled", &ScreenChannel::heightGridDdaEnabled);
     vector3("backgroundColor", &ScreenChannel::backgroundColor);
