81d885a5d08ee6c5ce2eb092ade6d28b4026bac0
zhangzachary834-commits <zhangzachary834@gmail.com>
2026-09-19 18:29:27 -0700

    Persist Relation semantic properties

--- src/Singularity/Storage/Serialization/Relation/RelationSerialization.cpp
+++ src/Singularity/Storage/Serialization/Relation/RelationSerialization.cpp
@@ -1,6 +1,7 @@
 #include "Singularity/Storage/Serialization/Relation/RelationSerialization.hpp"
 
 #include "ConstructedBeing/Singular/Lexeme/Lexeme.hpp"
+#include "Singularity/Storage/Serialization/Common/SingularPropertySerialization.hpp"
 
 #include <cstdio>
 #include <cstring>
@@ -76,6 +77,7 @@ nlohmann::json relationToJson(const Relation& relation) {
     if (relation.hasGroundedType()) {
         out["typeId"] = relation.type;
     }
+    Singularity::Storage::writeSingularProperties(out, relation);
     return out;
 }
 
@@ -122,5 +124,7 @@ Relation relationFromJson(const nlohmann::json& json,
                 relation.typeLabel().c_str(), savedA.c_str(), savedB.c_str());
         }
     }
+
+    Singularity::Storage::readSingularProperties(json, relation, resolve);
     return relation;
 }
