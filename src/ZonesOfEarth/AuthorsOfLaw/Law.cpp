390b76ff91af655029342eb1e52ed0c02ce28a31
zhangzachary834-commits <zhangzachary834@gmail.com>
2026-09-20 18:44:01 -0700

    Revoke LawManager callbacks during teardown

--- src/ZonesOfEarth/AuthorsOfLaw/Law.cpp
+++ src/ZonesOfEarth/AuthorsOfLaw/Law.cpp
@@ -1760,6 +1760,16 @@ void LawManager::add(const std::shared_ptr<Law>& law) {
 static LawManager* s_singularHookOwner = nullptr;
 
 LawManager::~LawManager() {
+    if (_ecaEventSubscription != 0) {
+        Core::EventBus::instance().unsubscribe<ECA::Event>(_ecaEventSubscription);
+        _ecaEventSubscription = 0;
+    }
+    if (_customEventSubscription != 0) {
+        Core::EventBus::instance().unsubscribe<Core::Event::Custom>(_customEventSubscription);
+        _customEventSubscription = 0;
+    }
+    _connected = false;
+
     if (s_singularHookOwner == this) {
         Singular::setPropertyChangeCallback(nullptr);
         Singular::setBeingReleasedCallback(nullptr);
@@ -1869,7 +1879,7 @@ void LawManager::connectToEventBus() {
         _dirty = true;
     });
 
-    Core::EventBus::instance().subscribe<ECA::Event>([this](const ECA::Event& e) {
+    _ecaEventSubscription = Core::EventBus::instance().subscribe<ECA::Event>([this](const ECA::Event& e) {
         std::string subjectId = e.subject ? e.subject->getIdentifier() : "null";
         std::string objectId = e.object ? e.object->getIdentifier() : "null";
 
@@ -1938,7 +1948,7 @@ void LawManager::connectToEventBus() {
         }
     });
 
-    Core::EventBus::instance().subscribe<Core::Event::Custom>([this](const Core::Event::Custom& e) {
+    _customEventSubscription = Core::EventBus::instance().subscribe<Core::Event::Custom>([this](const Core::Event::Custom& e) {
         if (!e.relation) return;
         
         std::string evType = e.relation->type;
