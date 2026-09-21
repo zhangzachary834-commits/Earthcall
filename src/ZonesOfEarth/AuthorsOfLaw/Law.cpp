cbf9280f7ed562616ff1bf8e7bbace3c6b3b3945
zhangzachary834-commits <zhangzachary834@gmail.com>
2026-09-20 19:35:58 -0700

    LawManager: close EventBus roads before teardown

--- src/ZonesOfEarth/AuthorsOfLaw/Law.cpp
+++ src/ZonesOfEarth/AuthorsOfLaw/Law.cpp
@@ -1808,6 +1808,19 @@ void LawManager::add(const std::shared_ptr<Law>& law) {
 static LawManager* s_singularHookOwner = nullptr;
 
 LawManager::~LawManager() {
+    // Close global EventBus roads while every member they can touch is still
+    // alive. EventBus also invalidates already-copied async snapshots before
+    // unsubscribe returns, so no delivery can enter this manager afterwards.
+    if (_ecaEventSubscription != 0) {
+        Core::EventBus::instance().unsubscribe(_ecaEventSubscription);
+        _ecaEventSubscription = 0;
+    }
+    if (_customEventSubscription != 0) {
+        Core::EventBus::instance().unsubscribe(_customEventSubscription);
+        _customEventSubscription = 0;
+    }
+    _connected = false;
+
     if (s_singularHookOwner == this) {
         Singular::setPropertyChangeCallback(nullptr);
         Singular::setBeingReleasedCallback(nullptr);
@@ -1918,7 +1931,7 @@ void LawManager::connectToEventBus() {
         _dirty = true;
     });
 
-    Core::EventBus::instance().subscribe<ECA::Event>([this](const ECA::Event& e) {
+    _ecaEventSubscription = Core::EventBus::instance().subscribe<ECA::Event>([this](const ECA::Event& e) {
         std::string subjectId = e.subject ? e.subject->getIdentifier() : "null";
         std::string objectId = e.object ? e.object->getIdentifier() : "null";
 
@@ -1987,7 +2000,7 @@ void LawManager::connectToEventBus() {
         }
     });
 
-    Core::EventBus::instance().subscribe<Core::Event::Custom>([this](const Core::Event::Custom& e) {
+    _customEventSubscription = Core::EventBus::instance().subscribe<Core::Event::Custom>([this](const Core::Event::Custom& e) {
         if (!e.relation) return;
         
         std::string evType = e.relation->type;
